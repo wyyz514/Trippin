cd8a7571875728c5bd2ea2a87fec175e0dca63a7
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-19 21:41:27 -0400

    Functions within the service return promises to the route's resolve

--- Trippin/www/js/services.js
+++ Trippin/www/js/services.js
@@ -1,10 +1,54 @@
 angular.module("app.services",[])
-.service("DataService",["$http",function($http){
-  return $http.get("/data/places.json")
-  .success(function(data){
-    return data;
-  })
-  .error(function(err){
-    return err;
-  });
+.service("DataService",["$q","$http",function($q,$http){
+  return {
+    getDestinations:function(){
+      return $http.get("/data/places.json")
+          .success(function(countries){
+            return countries;
+          })
+          .error(function(err){
+            return err;
+          });
+    },
+    getCities:function()
+    {
+      var defer = $q.defer();
+      this.getDestinations().then(function(data){
+        var cities = data.data.map(function(country){return country.cities;});
+        defer.resolve(cities);
+      });
+      return defer.promise;
+    },
+    getCity:function(city)
+    {
+      var found = "";
+      var defer = $q.defer();
+      var citiesPr = this.getCities();
+      var cities = "";
+      citiesPr.then(function(cts){
+        var _cities = cts.reduce(function(prev,curr){
+          return prev.concat(curr);
+        });
+        cities = _cities;
+        defer.resolve(find(city,cities));
+      });
+      
+      function find(city,cities)
+      {
+        var found = 0;
+        var len = cities.length;
+        for(var index = 0; index < len; index++)
+        {
+          if(cities[index].name == city)
+          {
+            found = cities[index];
+            break;
+          }
+        }
+        return found;
+      }
+      
+      return defer.promise;
+    }
+  };
 }]);
