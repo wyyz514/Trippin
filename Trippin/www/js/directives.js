dcc8303defea9eae445cf90845cfca32ba564450
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-21 18:07:34 -0400

    Decided to go with double binding in scope since it doesn't produce the duplicates error unlike the @ scoping rule

--- Trippin/www/js/directives.js
+++ Trippin/www/js/directives.js
@@ -3,9 +3,9 @@ angular.module("app.directives",[])
   return {
     restrict:"EA",
     scope:{
-      favorites:"="
+      favorites:"=favs"
     },
-    template:"<div class='item item-thumbnail-left item-text-wrap' ng-repeat='fav in favorites'><img src={{fav.image}}><h2>{{fav.name}}</h2><p>{{fav.description}}</p></div>",
+    template:"<div class='item item-thumbnail-left' ng-repeat='fav in favorites'><img src={{fav.image}}><h2>{{fav.name}}</h2><p>{{fav.description}}</p></div>",
     link:function(scope,el,attrs){
     
     }
