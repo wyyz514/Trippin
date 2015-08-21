dd58dbee38d772e64546bf7a8f88c7a7b1a9b6f9
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-21 17:06:33 -0400

    Controller declaration for city view. Manages favorites

--- Trippin/www/js/controllers.js
+++ Trippin/www/js/controllers.js
@@ -1,4 +1,9 @@
 angular.module("app.controllers",[])
 .controller("DestinationsCtrl",  ["$scope",function($scope){
   
+}])
+.controller("CityCtrl",["$scope","FavoritesService",function($scope,FavoritesService){
+  $scope.addFavorite = function(city){
+    console.log(FavoritesService.save(city));
+  };
 }]);
